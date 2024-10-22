a0895490bee12fbea25332ee3df4487c35e4c4e3
Swapna-Mandava <swapnam2512@gmail.com>
2024-10-22 14:58:07 +0530

    Assignment49

---
+++ assignments/Assignemnet49.java
@@ -0,0 +1,39 @@
+package assignments;
+
+public class Assignemnet49 {
+	public static void main(String[] args) {
+		
+		String input="Swapna";
+		
+		for(int i=0;i<input.length();i++) {
+			char c2=input.charAt(i);
+			
+		
+		
+			System.out.println(c2);
+		}
+		
+		for(int i=input.length()-1;i>=0;i--)
+		{
+		char c1=input.charAt(i);
+		
+		System.out.println("The result in reverse order---"+c1);
+		
+		System.out.println("");
+		}
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+	}
+
+}
